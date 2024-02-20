50c1058a72888b51bf8d13eed3a96a252a0451b1
jordanlkfung <jordanlkfung@gmail.com>
2024-02-19 23:53:01 -0800

    font and color change

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var questions = document.getElementsByClassName("questions");
-    for (var i = 0; i < questions.length; i++) {
-        questions[i].addEventListener("click", function () {
-            var answer = this.nextElementSibling;
-            if (answer.style.display === "block") {
-                answer.style.display = "none";
-            } else {
-                answer.style.display = "block";
-            }
-        });
-    }
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
 });
+
+const hiddenSections = document.querySelectorAll('.hidden');
+hiddenSections.forEach((section) => observer.observe(section));
+var questions = document.getElementsByClassName("questions");
+for (var i = 0; i < questions.length; i++) {
+    questions[i].addEventListener("click", function () {
+        var answer = this.nextElementSibling;
+        if (answer.style.display === "block") {
+            answer.style.display = "none";
+        } else {
+            answer.style.display = "block";
+        }
+    });
+}
+
