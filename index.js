f669b87abaf74befd0349ad1a1516631874df19a
jordanlkfung <jordanlkfung@gmail.com>
2024-02-19 19:10:32 -0800

    faq section

---
+++ index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var questions = document.getElementsByClassName("questions");
+    for (var i = 0; i < questions.length; i++) {
+        questions[i].addEventListener("click", function () {
+            var answer = this.nextElementSibling;
+            if (answer.style.display === "block") {
+                answer.style.display = "none";
+            } else {
+                answer.style.display = "block";
+            }
+        });
+    }
+});
